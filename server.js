b89112a0eacf86e7a4a1c7a8c7019201ca7e42b5
Soel M Sunny <soelsunny21@gmail.com>
2020-07-08 00:32:48 -0500

    npm i cookie-parser

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ const app = next({ dev });
 
 const handle = app.getRequestHandler();
 
+const AUTH_USER_TYPE = "authenticated";
+
+const authenticate = async (email, password) => {
+   const {data} = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+   return data.find(user =>{
+       if(user.email === email && user.website === password){
+           return user;
+       }
+   })
+}
+
 
 app.prepare().then(() =>{
     const server = express();
@@ -18,11 +30,25 @@ app.prepare().then(() =>{
 
     server.post('/api/login', (req, res) =>{
         const {email, password} = req.body;
-        res.json({
+
+        //echoing post data in server
+        /*res.json({
             email,
             password,
             success: true
-        });
+        });*/
+       
+        const user = await authenticate(email, password);
+        if(!user){
+            return res.status(403).send("Invalid email or password")  //unauthenticated status 403
+        }
+        const userData = {
+            name: user.name,
+            email: user.email,
+            type: AUTH_USER_TYPE
+        }
+        res.cookie();
+        res.json(userData);
     });
 
     server.get('*', (req, res)=>{
